afdd17ea420c1d01c983b08577846dc5d9af7450
Farooq Ahmad Ahmadi <farooqahmadahmadi1400@gmail.com>
2026-03-13 23:38:09 +0430

    AttendanceShiftsInfo model updated

--- backend/models/AttendanceShiftsInfo.js
+++ backend/models/AttendanceShiftsInfo.js
@@ -4,7 +4,6 @@ const sequelize = require("../config/db");
 const AttendanceShiftsInfo = sequelize.define(
     "AttendanceShiftsInfo",
     {
-
         attendance_shift_id: {
             type: DataTypes.BIGINT.UNSIGNED,
             primaryKey: true,
@@ -31,11 +30,25 @@ const AttendanceShiftsInfo = sequelize.define(
             type: DataTypes.TIME
         },
 
+        longitude: {
+            type: DataTypes.DECIMAL(10, 7),
+            allowNull: true
+        },
+
+        latitude: {
+            type: DataTypes.DECIMAL(10, 7),
+            allowNull: true
+        },
+
+        reduce: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+
         is_deleted: {
             type: DataTypes.BOOLEAN,
             defaultValue: false
         }
-
     },
     {
         tableName: "attendance_shifts_info",
