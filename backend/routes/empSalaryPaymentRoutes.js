3ff2d79f06565dd8f24ca06f64be44bac3db5ff1
Farooq Ahmad Ahmadi <farooqahmadahmadi1400@gmail.com>
2026-03-14 14:52:00 +0430

    emp salary payment secured

--- backend/routes/empSalaryPaymentRoutes.js
+++ backend/routes/empSalaryPaymentRoutes.js
@@ -1,12 +1,22 @@
+/* Access Controls
+Admin - Full Access
+HR - No Access
+Financial - Full Access
+Project Manager - No Access
+Employee - View Access (only their own payment records)
+Client - No Access
+*/
+
 const express = require("express");
 const router = express.Router();
 
 const empSalaryPaymentController = require("../controllers/empSalaryPaymentController");
+const role = require("../middlewares/roleMiddleware");
 
-router.get("/", empSalaryPaymentController.getAllPayments);
-router.get("/:id", empSalaryPaymentController.getPaymentById);
-router.post("/", empSalaryPaymentController.createPayment);
-router.put("/:id", empSalaryPaymentController.updatePayment);
-router.delete("/:id", empSalaryPaymentController.deletePayment);
+router.get("/", role("Admin", "Financial"), empSalaryPaymentController.getAllPayments);
+router.get("/:id", role("Admin", "Financial", "Employee"), empSalaryPaymentController.getPaymentById);
+router.post("/", role("Admin", "Financial"), empSalaryPaymentController.createPayment);
+router.put("/:id", role("Admin", "Financial"), empSalaryPaymentController.updatePayment);
+router.delete("/:id", role("Admin", "Financial"), empSalaryPaymentController.deletePayment);
 
 module.exports = router;
