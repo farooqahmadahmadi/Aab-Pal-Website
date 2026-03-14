be3e9043a378dd7c0ccc69c7494ee0039e106c3d
Farooq Ahmad Ahmadi <farooqahmadahmadi1400@gmail.com>
2026-03-14 14:49:18 +0430

    emp Aattend Secured

--- backend/routes/empAttendanceRoutes.js
+++ backend/routes/empAttendanceRoutes.js
@@ -1,12 +1,22 @@
+/* Access Controls
+Admin - Full Access
+HR - Full Access
+Financial - No Access
+Project Manager - No Access
+Employee - Create and View Access (Can only create and view their own attendance records)
+Client - No Access
+*/
+
 const express = require("express");
 const router = express.Router();
 
 const empAttendanceController = require("../controllers/empAttendanceController");
+const role = require("../middlewares/roleMiddleware");
 
-router.get("/", empAttendanceController.getAllAttendance);
-router.get("/:id", empAttendanceController.getAttendanceById);
-router.post("/", empAttendanceController.createAttendance);
-router.put("/:id", empAttendanceController.updateAttendance);
-router.delete("/:id", empAttendanceController.deleteAttendance);
+router.get("/", role("Admin", "HR"), empAttendanceController.getAllAttendance);
+router.get("/:id", role("Admin", "HR", "Employee"), empAttendanceController.getAttendanceById);
+router.post("/", role("Admin", "HR", "Employee"), empAttendanceController.createAttendance);
+router.put("/:id", role("Admin", "HR"), empAttendanceController.updateAttendance);
+router.delete("/:id", role("Admin", "HR"), empAttendanceController.deleteAttendance);
 
 module.exports = router;
