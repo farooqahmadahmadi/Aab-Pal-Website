333a37bbff757d06ec4f348380030537fcc39eae
Farooq Ahmad Ahmadi <farooqahmadahmadi1400@gmail.com>
2026-03-14 15:57:41 +0430

    Pro Phase Secured

--- backend/routes/projectPhasesRoutes.js
+++ backend/routes/projectPhasesRoutes.js
@@ -1,12 +1,22 @@
+/* Access Controls
+Admin - Full Access
+HR - No Access
+Financial - No Access
+Project Manager - Full Access
+Employee - No Access
+Client - View Access (Only for their projects)
+*/
+
 const express = require("express");
 const router = express.Router();
 
 const projectPhasesController = require("../controllers/projectPhasesController");
+const role = require("../middlewares/roleMiddleware");
 
-router.get("/", projectPhasesController.getAllPhases);
-router.get("/:id", projectPhasesController.getPhaseById);
-router.post("/", projectPhasesController.createPhase);
-router.put("/:id", projectPhasesController.updatePhase);
-router.delete("/:id", projectPhasesController.deletePhase);
+router.get("/", role("Admin", "Project Manager"), projectPhasesController.getAllPhases);
+router.get("/:id", role("Admin", "Project Manager", "Client"), projectPhasesController.getPhaseById);
+router.post("/", role("Admin", "Project Manager"), projectPhasesController.createPhase);
+router.put("/:id", role("Admin", "Project Manager"), projectPhasesController.updatePhase);
+router.delete("/:id", role("Admin","Project Manager"), projectPhasesController.deletePhase);
 
 module.exports = router;
