19cb4115b86aff778ded41ffeeef83aca3ca2438
Farooq Ahmad Ahmadi <farooqahmadahmadi1400@gmail.com>
2026-03-14 14:22:27 +0430

    departemnt secured

--- backend/routes/departmentInfoRoutes.js
+++ backend/routes/departmentInfoRoutes.js
@@ -1,12 +1,22 @@
+/* Access Controls
+Admin - Full Access
+HR - View Access
+Financial - No Access
+Project Manager - No Access
+Employee - No Access 
+Client - No Access
+*/
+
 const express = require("express");
 const router = express.Router();
 
 const departmentController = require("../controllers/departmentInfoController");
+const role = require("../middlewares/roleMiddleware");
 
-router.get("/", departmentController.getAllDepartments);
-router.get("/:id", departmentController.getDepartmentById);
-router.post("/", departmentController.createDepartment);
-router.put("/:id", departmentController.updateDepartment);
-router.delete("/:id", departmentController.deleteDepartment);
+router.get("/", role("Admin", "HR"), departmentController.getAllDepartments);
+router.get("/:id", role("Admin", "HR"), departmentController.getDepartmentById);
+router.post("/", role("Admin"), departmentController.createDepartment);
+router.put("/:id", role("Admin"), departmentController.updateDepartment);
+router.delete("/:id", role("Admin"), departmentController.deleteDepartment);
 
 module.exports = router;
