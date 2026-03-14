f12afa242a30b6e7576d2032009dd1d846c0e303
Farooq Ahmad Ahmadi <farooqahmadahmadi1400@gmail.com>
2026-03-14 16:07:47 +0430

    Equp Main Secured

--- backend/routes/equipmentMaintenanceRoutes.js
+++ backend/routes/equipmentMaintenanceRoutes.js
@@ -1,12 +1,22 @@
+/* Access Controls
+Admin - Full Access
+HR - No Access
+Financial - Full Access
+Project Manager - No Access
+Employee - No Access
+Client - No Access
+*/
+
 const express = require("express");
 const router = express.Router();
 
 const equipmentMaintenanceController = require("../controllers/equipmentMaintenanceController");
+const role = require("../middlewares/roleMiddleware");
 
-router.get("/", equipmentMaintenanceController.getAllMaintenance);
-router.get("/:id", equipmentMaintenanceController.getMaintenanceById);
-router.post("/", equipmentMaintenanceController.createMaintenance);
-router.put("/:id", equipmentMaintenanceController.updateMaintenance);
-router.delete("/:id", equipmentMaintenanceController.deleteMaintenance);
+router.get("/", role("Admin", "Financial"), equipmentMaintenanceController.getAllMaintenance);
+router.get("/:id", role("Admin", "Financial"), equipmentMaintenanceController.getMaintenanceById);
+router.post("/", role("Admin", "Financial"), equipmentMaintenanceController.createMaintenance);
+router.put("/:id", role("Admin", "Financial"), equipmentMaintenanceController.updateMaintenance);
+router.delete("/:id", role("Admin", "Financial"), equipmentMaintenanceController.deleteMaintenance);
 
 module.exports = router;
