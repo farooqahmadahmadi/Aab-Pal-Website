3538f819a79a9f561a31e2cc842ce47f5f15d614
Farooq Ahmad Ahmadi <farooqahmadahmadi1400@gmail.com>
2026-03-14 14:25:50 +0430

    emp secured

--- backend/routes/employeeInfoRoutes.js
+++ backend/routes/employeeInfoRoutes.js
@@ -1,12 +1,22 @@
+/* Access Controls
+Admin - Full Access
+HR - Full Access
+Financial - No Access
+Project Manager - No Access
+Employee - View self  Access 
+Client - No Access
+*/
+
 const express = require("express");
 const router = express.Router();
 
 const employeeController = require("../controllers/employeeInfoController");
+const role = require("../middlewares/roleMiddleware");
 
-router.get("/", employeeController.getAllEmployees);
-router.get("/:id", employeeController.getEmployeeById);
-router.post("/", employeeController.createEmployee);
-router.put("/:id", employeeController.updateEmployee);
-router.delete("/:id", employeeController.deleteEmployee);
+router.get("/", role("Admin", "HR"), employeeController.getAllEmployees);
+router.get("/:id", role("Admin", "HR", "Employee"), employeeController.getEmployeeById);
+router.post("/", role("Admin", "HR"), employeeController.createEmployee);
+router.put("/:id", role("Admin", "HR"), employeeController.updateEmployee);
+router.delete("/:id", role("Admin", "HR"), employeeController.deleteEmployee);
 
 module.exports = router;
