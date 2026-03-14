fa9cede410b93a43f13db42fc0b909c36f81e160
Farooq Ahmad Ahmadi <farooqahmadahmadi1400@gmail.com>
2026-03-14 16:11:57 +0430

    Equip usage secured

--- backend/routes/equipmentUsageRoutes.js
+++ backend/routes/equipmentUsageRoutes.js
@@ -1,12 +1,23 @@
+/* Access Controls
+Admin - Full Access
+HR - Create, Read Access
+Financial - Create, Read Access
+Project Manager - Create and Read Access
+Employee - No Access
+Client - No Access
+const role = require("../middlewares/roleMiddleware");
+*/
+
 const express = require("express");
 const router = express.Router();
 
 const equipmentUsageController = require("../controllers/equipmentUsageController");
+const role = require("../middlewares/roleMiddleware");
 
-router.get("/", equipmentUsageController.getAllUsage);
-router.get("/:id", equipmentUsageController.getUsageById);
-router.post("/", equipmentUsageController.createUsage);
-router.put("/:id", equipmentUsageController.updateUsage);
-router.delete("/:id", equipmentUsageController.deleteUsage);
+router.get("/", role("Admin", "HR", "Financial", "Project Manager"), equipmentUsageController.getAllUsage);
+router.get("/:id", role("Admin", "HR", "Financial", "Project Manager"), equipmentUsageController.getUsageById);
+router.post("/", role("Admin", "HR", "Financial", "Project Manager"), equipmentUsageController.createUsage);
+router.put("/:id", role("Admin", "HR", "Financial", "Project Manager"), equipmentUsageController.updateUsage);
+router.delete("/:id", role("Admin"), equipmentUsageController.deleteUsage);
 
 module.exports = router;
