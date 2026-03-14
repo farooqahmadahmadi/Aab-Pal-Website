97c573d8d6bf1c6d6c9997afc1b03333e331c086
Farooq Ahmad Ahmadi <farooqahmadahmadi1400@gmail.com>
2026-03-14 14:44:48 +0430

    Emp Hiring Secired

--- backend/routes/empHiringRoutes.js
+++ backend/routes/empHiringRoutes.js
@@ -1,11 +1,22 @@
+/* Access Controls
+Admin - Full Access
+HR - Full Access
+Financial - No Access
+Project Manager - No Access
+Employee - view Access (only their own hiring records)
+Client - No Access
+*/
+
 const express = require("express");
 const router = express.Router();
-const empHiringController = require("../controllers/empHiringController");
 
-router.get("/", empHiringController.getAllHirings);
-router.get("/:id", empHiringController.getHiringById);
-router.post("/", empHiringController.createHiring);
-router.put("/:id", empHiringController.updateHiring);
-router.delete("/:id", empHiringController.deleteHiring);
+const empHiringController = require("../controllers/empHiringController");
+const role = require("../middlewares/roleMiddleware");
+
+router.get("/", role("Admin", "HR"), empHiringController.getAllHirings);
+router.get("/:id", role("Admin", "HR", "Employee"), empHiringController.getHiringById);
+router.post("/", role("Admin", "HR"), empHiringController.createHiring);
+router.put("/:id", role("Admin", "HR"), empHiringController.updateHiring);
+router.delete("/:id", role("Admin","HR"), empHiringController.deleteHiring);
 
 module.exports = router;
