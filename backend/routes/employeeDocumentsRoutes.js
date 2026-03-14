4aadacf27bdeb2d02e0c76947e5a0002e22de06d
Farooq Ahmad Ahmadi <farooqahmadahmadi1400@gmail.com>
2026-03-14 14:28:12 +0430

    emp doc updated

--- backend/routes/employeeDocumentsRoutes.js
+++ backend/routes/employeeDocumentsRoutes.js
@@ -1,12 +1,23 @@
+/* Access Controls
+Admin - Full Access
+HR - Full Access
+Financial - No Access
+Project Manager - No Access
+Employee - View Self Access
+Client - No Access
+const role = require("../middlewares/roleMiddleware");
+*/
+
 const express = require("express");
 const router = express.Router();
 
 const employeeDocumentsController = require("../controllers/employeeDocumentsController");
+const role = require("../middlewares/roleMiddleware");
 
-router.get("/", employeeDocumentsController.getAllDocuments);
-router.get("/:id", employeeDocumentsController.getDocumentById);
-router.post("/", employeeDocumentsController.createDocument);
-router.put("/:id", employeeDocumentsController.updateDocument);
-router.delete("/:id", employeeDocumentsController.deleteDocument);
+router.get("/", role("Admin", "HR"), employeeDocumentsController.getAllDocuments);
+router.get("/:id", role("Admin", "HR", "Employee"), employeeDocumentsController.getDocumentById);
+router.post("/", role("Admin", "HR"), employeeDocumentsController.createDocument);
+router.put("/:id", role("Admin", "HR"), employeeDocumentsController.updateDocument);
+router.delete("/:id", role("Admin", "HR"), employeeDocumentsController.deleteDocument);
 
 module.exports = router;
