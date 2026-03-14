d590bf4abf52a590a0a4fb4c8d6abf62598ceb31
Farooq Ahmad Ahmadi <farooqahmadahmadi1400@gmail.com>
2026-03-14 15:17:31 +0430

    Client Secured

--- backend/routes/clientRoutes.js
+++ backend/routes/clientRoutes.js
@@ -1,12 +1,22 @@
+/* Access Controls
+Admin - Full Access
+HR - No Access
+Financial - No Access
+Project Manager - View Access
+Employee - No Access 
+Client - View Access (Only their own data)
+*/
+
 const express = require("express");
 const router = express.Router();
 
 const clientController = require("../controllers/clientController");
+const role = require("../middlewares/roleMiddleware");
 
-router.get("/", clientController.getAllClients);
-router.get("/:id", clientController.getClientById);
-router.post("/", clientController.createClient);
-router.put("/:id", clientController.updateClient);
-router.delete("/:id", clientController.deleteClient);
+router.get("/", role("Admin", "Project Manager", "Client"), clientController.getAllClients);
+router.get("/:id", role("Admin", "Project Manager", "Client"), clientController.getClientById);
+router.post("/", role("Admin"), clientController.createClient);
+router.put("/:id", role("Admin"), clientController.updateClient);
+router.delete("/:id", role("Admin"), clientController.deleteClient);
 
 module.exports = router;
