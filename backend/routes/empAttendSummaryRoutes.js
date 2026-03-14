ae683f5fb27f89c60840c4768fc84d4752b60d66
Farooq Ahmad Ahmadi <farooqahmadahmadi1400@gmail.com>
2026-03-14 14:54:54 +0430

    emp ateens sum seured

--- backend/routes/empAttendSummaryRoutes.js
+++ backend/routes/empAttendSummaryRoutes.js
@@ -1,12 +1,22 @@
+/* Access Controls
+Admin - Full Access
+HR - Full Access
+Financial - View Access 
+Project Manager - No Access
+Employee - View Access (Only their own summary records)
+Client - No Access
+*/
+
 const express = require("express");
 const router = express.Router();
 
 const empAttendSummaryController = require("../controllers/empAttendSummaryController");
+const role = require("../middlewares/roleMiddleware");
 
-router.get("/", empAttendSummaryController.getAllSummaries);
-router.get("/:id", empAttendSummaryController.getSummaryById);
-router.post("/", empAttendSummaryController.createSummary);
-router.put("/:id", empAttendSummaryController.updateSummary);
-router.delete("/:id", empAttendSummaryController.deleteSummary);
+router.get("/", role("Admin", "HR", "Financial"), empAttendSummaryController.getAllSummaries);
+router.get("/:id", role("Admin", "HR", "Financial", "Employee"), empAttendSummaryController.getSummaryById);
+router.post("/", role("Admin", "HR"), empAttendSummaryController.createSummary);
+router.put("/:id", role("Admin", "HR"), empAttendSummaryController.updateSummary);
+router.delete("/:id", role("Admin", "HR"), empAttendSummaryController.deleteSummary);
 
 module.exports = router;
