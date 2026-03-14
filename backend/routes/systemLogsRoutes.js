144f7f5d5bf0d7983b4509a819e16c7cad35caa5
Farooq Ahmad Ahmadi <farooqahmadahmadi1400@gmail.com>
2026-03-14 20:42:28 +0430

    log secured

--- backend/routes/systemLogsRoutes.js
+++ backend/routes/systemLogsRoutes.js
@@ -1,12 +1,22 @@
+/* Access Controls
+Admin - Full Access
+HR - No Access
+Financial - No Access
+Project Manager - No Access
+Employee - No Access
+Client - No Access
+*/
+
 const express = require("express");
 const router = express.Router();
 
 const systemLogsController = require("../controllers/systemLogsController");
+const role = require("../middlewares/roleMiddleware");
 
-router.get("/", systemLogsController.getAllLogs);
-router.get("/:id", systemLogsController.getLogById);
-router.post("/", systemLogsController.createLog);
-router.put("/:id", systemLogsController.updateLog);
-router.delete("/:id", systemLogsController.deleteLog);
+router.get("/", role("Admin"), systemLogsController.getAllLogs);
+router.get("/:id", role("Admin"), systemLogsController.getLogById);
+router.post("/", role("Admin"), systemLogsController.createLog);
+//router.put("/:id", role("Admin"), systemLogsController.updateLog);
+router.delete("/:id", role("Admin"), systemLogsController.deleteLog);
 
 module.exports = router;
