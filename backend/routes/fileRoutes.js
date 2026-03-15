fd2ae1b0424d371fcfa96ee2f4bf25cec22d4655
Farooq Ahmad Ahmadi <farooqahmadahmadi1400@gmail.com>
2026-03-16 00:17:59 +0430

    upload updated

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -7,6 +7,9 @@ const role = require("../middlewares/roleMiddleware");
 // Upload: Admin, HR, Project Manager
 router.post("/upload", role("Admin", "HR", "Project Manager"), upload.single("file"), fileController.uploadFile);
 
+// Update: Admin, HR, Project Manager
+router.put("/update", role("Admin", "HR", "Project Manager"), upload.single("file"), fileController.updateFile);
+
 // Download: All roles (if access allowed)
 router.get("/download/:section/:filename", role("Admin", "HR", "Project Manager", "Employee", "Client"), fileController.downloadFile);
 
