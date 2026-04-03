d3d11365c2da7f8a47b3040294eddc8f90e3264a
Farooq Ahmad Ahmadi <farooqahmadahmadi1400@gmail.com>
2026-04-03 23:59:08 +0430

    ok

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,     // Database user
   process.env.DB_PASSWORD, // Database password
   {
-    host: process.env.DB_HOST, // Database host
+    host: process.env.DB_HOST,  // Database host
     dialect: "mysql",           // MySQL
     logging: false,             // Console logging off
   }
