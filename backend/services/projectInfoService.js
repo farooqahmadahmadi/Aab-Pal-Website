d062fe0285f4b98f69086a6a0ce6c2660529a522
Farooq Ahmad Ahmadi <farooqahmadahmadi1400@gmail.com>
2026-04-09 09:57:11 -0700

    Project Info Log HARD & Soft Delete completed

--- backend/services/projectInfoService.js
+++ backend/services/projectInfoService.js
@@ -1,43 +1,101 @@
 const ProjectInfo = require("../models/ProjectInfo");
-const { Op } = require("sequelize");
+const Client = require("../models/ClientInfo");
+const Employee = require("../models/EmployeeInfo");
+const logService = require("./systemLogsService");
+const { handleDelete } = require("../utils/deleteHelper");
 
-// Get all projects
+// 🔥 helper
+const getUserId = (user) => user?.user_id || user?.id || 0;
+
+// ===== GET ALL =====
 const getAllProjects = async () => {
-    return await ProjectInfo.findAll({ where: { is_deleted: false } });
+  return await ProjectInfo.findAll({
+    where: { is_deleted: false },
+    order: [["created_at", "DESC"]],
+  });
 };
 
-// Add new project
-const addProject = async (data) => {
-    return await ProjectInfo.create(data);
+// ===== GET BY ID =====
+const getProjectById = async (id) => {
+  return await ProjectInfo.findOne({
+    where: { project_id: id, is_deleted: false },
+  });
 };
 
-// Update project
-const updateProject = async (id, data) => {
-    return await ProjectInfo.update(data, { where: { project_id: id } });
+// ===== CREATE =====
+const addProject = async (data, user = {}) => {
+  const project = await ProjectInfo.create(data);
+
+  await logService.createLog({
+    user_id: getUserId(user),
+    action: "CREATE",
+    reference_table: "project_info",
+    reference_record_id: project.project_id,
+    old_value: null,
+    new_value: project.toJSON(),
+  });
+
+  return project;
 };
 
-// Delete project (soft delete)
-const deleteProject = async (id) => {
-    return await ProjectInfo.update({ is_deleted: true }, { where: { project_id: id } });
+// ===== UPDATE =====
+const updateProject = async (id, data, user = {}) => {
+  const project = await ProjectInfo.findOne({
+    where: { project_id: id, is_deleted: false },
+  });
+
+  if (!project) throw new Error("Project not found");
+
+  const oldValue = project.toJSON();
+
+  await project.update(data);
+
+  await logService.createLog({
+    user_id: getUserId(user),
+    action: "UPDATE",
+    reference_table: "project_info",
+    reference_record_id: project.project_id,
+    old_value: oldValue,
+    new_value: project.toJSON(),
+  });
+
+  return project;
 };
 
-// Dropdowns
+// ===== DELETE (Soft + Hard via helper) =====
+const deleteProject = async (id, user = {}) => {
+  const project = await ProjectInfo.findOne({
+    where: { project_id: id, is_deleted: false },
+  });
+
+  if (!project) throw new Error("Project not found");
+
+  await handleDelete(project, user, "project_info", getUserId(user));
+
+  return true;
+};
+
+// ===== DROPDOWNS =====
 const getClients = async () => {
-    // فرضي: بیا باید ClientInfo model جوړ وي
-    const Client = require("../models/ClientInfo");
-    return await Client.findAll({ where: { is_deleted: false } });
+  return await Client.findAll({
+    where: { is_deleted: false },
+    order: [["created_at", "DESC"]],
+  });
 };
 
 const getEmployees = async () => {
-    const Employee = require("../models/EmployeeInfo");
-    return await Employee.findAll({ where: { is_deleted: false } });
+  return await Employee.findAll({
+    where: { is_deleted: false },
+    order: [["created_at", "DESC"]],
+  });
 };
 
 module.exports = {
-    getAllProjects,
-    addProject,
-    updateProject,
-    deleteProject,
-    getClients,
-    getEmployees
+  getAllProjects,
+  getProjectById,
+  addProject,
+  updateProject,
+  deleteProject,
+  getClients,
+  getEmployees,
 };
