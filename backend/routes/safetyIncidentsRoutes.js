98c0b5ab61040af26ed2bd93b101c3c52537bd27
Farooq Ahmad Ahmadi <farooqahmadahmadi1400@gmail.com>
2026-04-10 09:26:15 -0700

    Safety incidents log, hard nad soft delete complted

--- backend/routes/safetyIncidentsRoutes.js
+++ backend/routes/safetyIncidentsRoutes.js
@@ -1,9 +1,18 @@
 const router = require("express").Router();
-const controller = require("../controllers/safetyIncidentsController");
 
-router.get("/", controller.getAll);
-router.post("/", controller.create);
-router.put("/:id", controller.update);
-router.delete("/:id", controller.remove);
+const controller = require("../controllers/safetyIncidentsController");
+const { authMiddleware } = require("../middlewares/authMiddleware");
+
+// ===== GET ALL =====
+router.get("/", authMiddleware, controller.getAll);
+
+// ===== CREATE =====
+router.post("/", authMiddleware, controller.create);
+
+// ===== UPDATE =====
+router.put("/:id", authMiddleware, controller.update);
+
+// ===== DELETE =====
+router.delete("/:id", authMiddleware, controller.remove);
 
 module.exports = router;
