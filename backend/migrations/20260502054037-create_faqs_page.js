f1c9b11eb3cb735e4e3942b5793f377e4f954865
Farooq Ahmad Ahmadi <farooqahmadahmadi1400@gmail.com>
2026-05-01 22:43:41 -0700

    FAQ Migration an Model Crated

---
+++ backend/migrations/20260502054037-create_faqs_page.js
@@ -0,0 +1,60 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('faqs_page', {
+      faqs_id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true
+      },
+
+      language_id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: false,
+        references: {
+          model: 'website_languages',
+          key: 'language_id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+
+      faqs_question: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+
+      faqs_answer: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+
+      faqs_category: {
+        type: Sequelize.STRING(100),
+        allowNull: true
+      },
+
+      is_active: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+      }
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('faqs_page');
+  }
+};
--- backend/models/index.js
+++ backend/models/index.js
@@ -3,6 +3,7 @@ const WebsiteLanguage = require("./WebsiteLanguage");
 const HomePage = require("./HomePage");
 const BlogsPage = require("./BlogsPage");
 const BlogComments = require("./BlogComments");
+const FaqsPage = require("./FaqsPage");
 
 // Home Page → Website Languages
 HomePage.belongsTo(WebsiteLanguage, { foreignKey: "language_id" });
@@ -16,10 +17,14 @@ WebsiteLanguage.hasMany(BlogsPage, { foreignKey: "language_id" });
 BlogsPage.hasMany(BlogComments, { foreignKey: "blog_id" });
 BlogComments.belongsTo(BlogsPage, { foreignKey: "blog_id" });
 
+// FAQs Page → Website Languages
+FaqsPage.belongsTo(WebsiteLanguage, { foreignKey: "language_id" });
+WebsiteLanguage.hasMany(FaqsPage, { foreignKey: "language_id" });
 module.exports = {
   sequelize,
   WebsiteLanguage,
   HomePage,
   BlogsPage,
   BlogComments,
+  FaqsPage,
 };
